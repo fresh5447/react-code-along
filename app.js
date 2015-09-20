d0f61a7a61f5206eea6f1668522842b7f4467993
Doug Walter <doug@kosmojo.com>
2015-09-19 18:36:45 -0600

    add product table, prod cat, prod row

--- app.js
+++ app.js
@@ -1,3 +1,50 @@
+var ProductCategoryRow = React.createClass({
+    render: function() {
+        return (<tr><th colSpan="2">{this.props.category}</th></tr>);
+    }
+});
+
+var ProductRow = React.createClass({
+    render: function() {
+        var name = this.props.product.stocked ?
+            this.props.product.name :
+            <span style={{color: 'red'}}>
+                {this.props.product.name}
+            </span>;
+        return (
+            <tr>
+                <td>{name}</td>
+                <td>{this.props.product.price}</td>
+            </tr>
+        );
+    }
+});
+
+var ProductTable = React.createClass({
+    render: function() {
+        var rows = [];
+        var lastCategory = null;
+        this.props.products.forEach(function(product) {
+            if (product.category !== lastCategory) {
+                rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+            }
+            rows.push(<ProductRow product={product} key={product.name} />);
+            lastCategory = product.category;
+        });
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>{rows}</tbody>
+            </table>
+        );
+    }
+});
+
 var SearchBar = React.createClass({
     render: function() {
         return (
@@ -18,7 +65,7 @@ var FilterableProductTable = React.createClass({
         return (
             <div>
             <SearchBar />
-                <h1> Hello world </h1>
+            <ProductTable products={this.props.products} />
             </div>
         );
     }
